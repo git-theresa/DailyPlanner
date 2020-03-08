dd9dbf9d456fcbc03375d22fdd45df761e7706a8
Theresa E <t.eatherly@gmail.com>
2020-03-08 09:56:49 -0500

    save button appended in console log working

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ $( document ).ready(function(){
    $("#currentDay").append(now);
 
 
+ for (var i = 0; i < 8; i++) {
+    var inputBox = $("<input>");
+    console.log(inputBox);
+$(".plannerContainer").append(inputBox);
+$(inputBox).attr("class", "row");
+    var saveMe = $("<button>");
+$("inputBox").append($(saveMe));
+console.log(saveMe);
 
 
 
+
+
+
+}
 });
