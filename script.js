62a7c87d60fa7957befbe806828a3daf96078857
Theresa E <t.eatherly@gmail.com>
2020-03-09 18:37:50 -0500

    added row before columns

--- script.js
+++ script.js
@@ -4,73 +4,67 @@ $( document ).ready(function(){
    $("#currentDay").append(now);
 
 
-    for (var i = 0; i < 8; i++) {
-    var inputBox = $("<input>");
+
+
+// var columnHourDiv = $("<div>");
+// columnHourDiv.addClass("col-md-2");
+// var columnHourSpan = $("<span>");
+// columnHourSpan.attr("class", currentHour);
+
+
+// columnHourSpan.text(displayHour);
+// // adjust column span text
+// rowDiv.append(columnHourDiv);
+// columnHourDiv.append(displayHour);
+
+    for (var i = 0; i < 4; i++) {
+var rowDiv = $("<div>");
+rowDiv.attr("class", "row");
+rowDiv = $("hour" + "inputBox" +  "saveBtn");
+ console.log(rowDiv);
+
+    var inputBox = $("<div>");
     console.log(inputBox);
-    $(inputBox).attr("class", "input-group form-control mb-3", "type", "text");
+    $(inputBox).attr("class", "input-group form-control mb-3  col-md-9", "type", "text");
     $(".plannerContainer").append(inputBox);
-    // $(".plannerContainer").append(columnInputDiv);
-    // $(".plannerContainer").append(columnHourDiv);
-    // $(".plannerContainer").append(columnSaveDiv);
-   
-    var columnInputDiv = $("<div>");
-    columnInputDiv.attr("class","col-md-6");
-
-    
-
-    var columnSaveDiv = $("<div>");
-    columnSaveDiv.attr("col-md-2");
- 
-
+  
     var saveBtn = $("<i>");
-    saveBtn.attr("class","input-group-append  input-group-text btn-primary fa fa-save");
+    saveBtn.attr("class","input-group-append  input-group-text col-md-1  btn-primary fa fa-save");
     console.log(saveBtn);
-    inputBox.after(saveBtn);
+    $(inputBox).after(saveBtn);
    
     
-  var hour = document.createElement("input");
-  hour.setAttribute("class", "input-group-prepend data-time");
-  console.log(hour);
-  inputBox.before(hour);
-   
-}    
+    var hour = $("<div>");
+    hour.attr("class", "input-group-prepend col-md-2");
+    console.log(hour);
+    $(inputBox).after(hour);
+//  $("hour").append(currentHour);
+    }    
 
 var currentHour = moment().format("h");
 var displayHour = 0;
 var amPm= "";
 
-
-
-var rowDiv = $("<div>");
-rowDiv.attr("class", "row");
-rowDiv.attr("class", "inputRow");
-rowDiv.attr("class", "hour-index", currentHour);
-
-var columnHourDiv = $("<div>");
-    columnHourDiv.addClass("col-md-2");
-var columnHourSpan = $("<span>");
-columnHourSpan.attr("class", currentHour);
-
 if (currentHour > 12) {
-displayHour = currentHour - 12;
+displayHour = currentHour + 12;
 amPm = "pm";
 }
 else{
         displayHour = currentHour;
     amPm = "am";
+    console.log(displayHour);
+    console.log(amPm);
 };
 
-columnHourSpan.text("${displayHour}${amPm");
-rowDiv.append(columnHourDiv);
-columnHourDiv.append(columnHourSpan);
+
     // // This function handles events where one button is clicked
     // $("saveBtn").on("click", function(event) {
     //     // Preventing the buttons default behavior when clicked (which is submitting a form)
     //     event.preventDefault();
-    //     // This line grabs the input from the textbox
+    //     // This line grabs the input from the textBox
     //     var movie = $("#movie-input").val().trim();
     //     var toDo = $("user-input").val().trim();
-    //     // Adding the movie from the textbox to our array
+    //     // Adding the movie from the textBox to our array
     //     movies.push(movie);
     //     toDoList.push(toDo);
     //     // Calling renderButtons which handles the processing of our movie array
@@ -91,6 +85,10 @@ columnHourDiv.append(columnHourSpan);
 //             '</div>';
 //         blocks += hourBlock;
 //         console.log(hourBlock);
+
+
+
+
 //     }       
 //      $("#timeBlocks").prepend(blocks);
  
