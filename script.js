99408d130f5a7d7705994aa2524b10af69c6e706
Theresa E <t.eatherly@gmail.com>
2020-03-08 22:53:20 -0500

    hourEl / preppend and append buttons

--- script.js
+++ script.js
@@ -4,19 +4,57 @@ $( document ).ready(function(){
    $("#currentDay").append(now);
 
 
- for (var i = 0; i < 8; i++) {
-    var inputBox = $("<input>");
+    for (var i = 0; i < 8; i++) {
+    var $inputBox = $("<input>");
     console.log(inputBox);
-$(".plannerContainer").append(inputBox);
-$(inputBox).attr("class", "row");
-    var saveMe = $("<button>");
-$("inputBox").append($(saveMe));
-console.log(saveMe);
+    $(inputBox).attr("class", "input-group form-control mb-3");
+    $(".plannerContainer").append(inputBox);
+   
+      
+    var saveBtn = $("<button>");
+    $(saveBtn).attr("class","input-group-append column-flex btn-primary input-group-append fa fa-save");
+    console.log(saveBtn);
+    $(".plannerContainer").append($(saveBtn)).after(inputBox);
+     
+    
+  var hour = $("<input>");
+  $(hour).attr("class", "input-group-prepend data-time");
+  $("#hour").prepend($(inputBox)).before(inputBox);
 
 
+    }
+    // // This function handles events where one button is clicked
+    // $("saveBtn").on("click", function(event) {
+    //     // Preventing the buttons default behavior when clicked (which is submitting a form)
+    //     event.preventDefault();
+    //     // This line grabs the input from the textbox
+    //     var movie = $("#movie-input").val().trim();
+    //     var toDo = $("user-input").val().trim();
+    //     // Adding the movie from the textbox to our array
+    //     movies.push(movie);
+    //     toDoList.push(toDo);
+    //     // Calling renderButtons which handles the processing of our movie array
+    //     renderButtons();
 
-
-
+createTimeBlocks = () => {
+    let blocks = "";
+    for (let i = 0; i < workHours.length; i++) {
+        const hourBlock = "" +
+            '<div class="input-group mb-3">' +
+            '<div class="input-group-prepend">' +
+            '<span class="input-group-text">' + workHours[i] + '</span>' +
+            '</div>' +
+            '<input type="text" class="form-control" id="' + workHours[i] + '">' +
+            '<div class="input-group-append">' +
+            '<button class="btn btn-primary" data-time="' + workHours[i] + '"><i class="fa fa-save"></i></button>' +
+            '</div>' +
+            '</div>';
+        blocks += hourBlock;
+        console.log(hourBlock);
+    }       
+     $("#timeBlocks").prepend(blocks);
+ 
 
 }
+
 });
