1399d5ed6a7dcf45ef3f9979a92239e274d330c2
Theresa E <t.eatherly@gmail.com>
2020-03-10 10:03:44 -0500

    inputBox bootstrap in one div

--- script.js
+++ script.js
@@ -1,98 +1,82 @@
-$( document ).ready(function(){
-    var now = moment().format("LLLL");
-    console.log(now);
-   $("#currentDay").append(now);
-
-
-
-
-// var columnHourDiv = $("<div>");
-// columnHourDiv.addClass("col-md-2");
-// var columnHourSpan = $("<span>");
-// columnHourSpan.attr("class", currentHour);
-
-
-// columnHourSpan.text(displayHour);
-// // adjust column span text
-// rowDiv.append(columnHourDiv);
-// columnHourDiv.append(displayHour);
-
-    for (var i = 0; i < 4; i++) {
-var rowDiv = $("<div>");
-rowDiv.attr("class", "row");
-rowDiv = $("hour" + "inputBox" +  "saveBtn");
- console.log(rowDiv);
-
-    var inputBox = $("<div>");
-    console.log(inputBox);
-    $(inputBox).attr("class", "input-group form-control mb-3  col-md-9", "type", "text");
-    $(".plannerContainer").append(inputBox);
-  
-    var saveBtn = $("<i>");
-    saveBtn.attr("class","input-group-append  input-group-text col-md-1  btn-primary fa fa-save");
-    console.log(saveBtn);
-    $(inputBox).after(saveBtn);
-   
+$(document).ready(function() {
     
-    var hour = $("<div>");
-    hour.attr("class", "input-group-prepend col-md-2");
-    console.log(hour);
-    $(inputBox).after(hour);
-//  $("hour").append(currentHour);
-    }    
+  var now = moment().format("LLLL");
+  console.log(now);
+  $("#currentDay").append(now);
 
-var currentHour = moment().format("h");
-var displayHour = 0;
-var amPm= "";
+  var colorTime = "";
+  var hours = ["09", "10", "11", "12", "1", "2", "3", "4", "5"];
 
-if (currentHour > 12) {
-displayHour = currentHour + 12;
-amPm = "pm";
-}
-else{
-        displayHour = currentHour;
+  for (var i = 0; i < hours.length; i++) {
+    var inputBox = $(`<div class="input-group mb-3 ${colorTime}"><div class="input-group-prepend">
+    <span class="input-group-text">${hours[i]}:00</span>
+      </div>
+      <input type="text" class="form-control user-input ${colorTime}">
+      <div class="input-group-append">
+        <span class="input-group-text"><button class = "saveBtn" data-time="${hours[i]}"><i class= 'fas fa-save'></i></span>
+      </div></div>`);
+    $(".plannerContainer").append(inputBox);
+  }
+  console.log(inputBox);
+
+  var hours = moment().format("h");
+  var displayHour = 0;
+  var amPm = "";
+
+  if (hours > 12) {
+    displayHour = hours + 12;
+    amPm = "pm";
+  } else {
+    displayHours = hours;
     amPm = "am";
-    console.log(displayHour);
+    console.log(displayHours);
     console.log(amPm);
-};
+  }
 
+  function renderUserInput() {
+    var userInput = localStorage.getItem("userInput");
+    if (userInput === null) {
+      return;
+    }
+    userInputSpan.textContent = userInput;
+    //renderUserInput();
+  }
 
-    // // This function handles events where one button is clicked
-    // $("saveBtn").on("click", function(event) {
-    //     // Preventing the buttons default behavior when clicked (which is submitting a form)
-    //     event.preventDefault();
-    //     // This line grabs the input from the textBox
-    //     var movie = $("#movie-input").val().trim();
-    //     var toDo = $("user-input").val().trim();
-    //     // Adding the movie from the textBox to our array
-    //     movies.push(movie);
-    //     toDoList.push(toDo);
-    //     // Calling renderButtons which handles the processing of our movie array
-    //     renderButtons();
+  // // This function handles events where one button is clicked
+  $(".saveBtn").on("click", function(event) {
+    event.preventDefault();
 
-// createTimeBlocks = () => {
-//     let blocks = "";
-//     for (let i = 0; i < workHours.length; i++) {
-//         const hourBlock = "" +
-//             '<div class="input-group mb-3">' +
-//             '<div class="input-group-prepend">' +
-//             '<span class="input-group-text">' + workHours[i] + '</span>' +
-//             '</div>' +
-//             '<input type="text" class="form-control" id="' + workHours[i] + '">' +
-//             '<div class="input-group-append">' +
-//             '<button class="btn btn-primary" data-time="' + workHours[i] + '"><i class="fa fa-save"></i></button>' +
-//             '</div>' +
-//             '</div>';
-//         blocks += hourBlock;
-//         console.log(hourBlock);
+    console.log("Save button has been clicked");
+    var toDo = $(".user-input")
+      .val()
+      .trim();
+    console.log(toDo);
 
+    localStorage.setItem($(this).attr('data-time'), toDo)
 
+    // save todo to localstorage
 
+   // toDoList.push(toDo);
+   // renderUserInput();
+  });
 
-//     }       
-//      $("#timeBlocks").prepend(blocks);
- 
+  // createTimeBlocks = () => {
+  //     let blocks = "";
+  //     for (let i = 0; i < workHours.length; i++) {
+  //         const hourBlock = "" +
+  //             '<div class="input-group mb-3">' +
+  //             '<div class="input-group-prepend">' +
+  //             '<span class="input-group-text">' + workHours[i] + '</span>' +
+  //             '</div>' +
+  //             '<input type="text" class="form-control" id="' + workHours[i] + '">' +
+  //             '<div class="input-group-append">' +
+  //             '<button class="btn btn-primary" data-time="' + workHours[i] + '"><i class="fa fa-save"></i></button>' +
+  //             '</div>' +
+  //             '</div>';
+  //         blocks += hourBlock;
+  //         console.log(hourBlock);
 
-// }
-
+  //     }
+  // }
+  //      $("#timeBlocks").prepend(blocks);
 });
