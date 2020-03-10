5e301db06fe470b4222fe3cb7c520283dcf05973
Theresa E <t.eatherly@gmail.com>
2020-03-10 17:06:01 -0500

    colorTime blocks are functioning for (future)

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 $(document).ready(function() {
-    
   var now = moment().format("LLLL");
   console.log(now);
   $("#currentDay").append(now);
 
-  var colorTime = "";
+    var hours = moment().format("h");
+  
   var hours = ["09", "10", "11", "12", "1", "2", "3", "4", "5"];
-
+//   ".present" is pink $("inputBox").css("background-color", );
   for (var i = 0; i < hours.length; i++) {
-    var inputBox = $(`<div class="input-group mb-3 ${colorTime}"><div class="input-group-prepend">
+    var colorTime;
+    if (moment().hours() === hours[i]) {
+      colorTime = "present";
+    }
+    else if (moment().hours() > hours[i]) {
+      colorTime = "future";
+    } 
+     else if (moment().hours() < hours[i]) {
+      colorTime = "past";
+    }
+    var inputBox = $(`<div class="input-group mb-3"${colorTime}'><div class="input-group-prepend">
     <span class="input-group-text">${hours[i]}:00</span>
       </div>
+
       <input type="text" class="form-control user-input ${colorTime}">
       <div class="input-group-append">
-        <span class="input-group-text"><button class = "saveBtn" data-time="${hours[i]}"><i class= 'fas fa-save'></i></span>
-      </div></div>`);
+        <span class="input-group-text">
+        <button class = "saveBtn" data-time="${hours[i]}"><i class= 'fas fa-save'></i>
+        </span>
+      </div>
+      </div>`);
     $(".plannerContainer").append(inputBox);
   }
-  console.log(inputBox);
+ // save color changes
+  // css classes ".past" is white  / ".present" is pink   / ".future" is green #77dd77
+//   $("inputBox").mouseover(function(){
+//       $("inputBox").css("background-color", "#ff6961");
+//     }
+//     // $("inputBox").mouseout(function(){
+//     //   $("inputBox").css("background-color", "#d3d3d3");
+//     // }
+//   console.log(inputBox);
 
-  var hours = moment().format("h");
-  var displayHour = 0;
+  
+  var displayHours = 0;
   var amPm = "";
 
   if (hours > 12) {
-    displayHour = hours + 12;
+    displayHours = hours + 12;
     amPm = "pm";
   } else {
     displayHours = hours;
@@ -32,7 +54,7 @@ $(document).ready(function() {
     console.log(displayHours);
     console.log(amPm);
   }
-
+ userInput = (" ");
   function renderUserInput() {
     var userInput = localStorage.getItem("userInput");
     if (userInput === null) {
@@ -41,42 +63,22 @@ $(document).ready(function() {
     userInputSpan.textContent = userInput;
     //renderUserInput();
   }
-
+   
   // // This function handles events where one button is clicked
   $(".saveBtn").on("click", function(event) {
     event.preventDefault();
 
     console.log("Save button has been clicked");
-    var toDo = $(".user-input")
-      .val()
-      .trim();
-    console.log(toDo);
+    var userInput = $(".userInput").val();
+    console.log(userInput);
 
-    localStorage.setItem($(this).attr('data-time'), toDo)
 
-    // save todo to localstorage
+    localStorage.setItem($(this).attr("data-time"), userInput);
 
-   // toDoList.push(toDo);
-   // renderUserInput();
+   
+    renderUserInput();
   });
+ 
 
-  // createTimeBlocks = () => {
-  //     let blocks = "";
-  //     for (let i = 0; i < workHours.length; i++) {
-  //         const hourBlock = "" +
-  //             '<div class="input-group mb-3">' +
-  //             '<div class="input-group-prepend">' +
-  //             '<span class="input-group-text">' + workHours[i] + '</span>' +
-  //             '</div>' +
-  //             '<input type="text" class="form-control" id="' + workHours[i] + '">' +
-  //             '<div class="input-group-append">' +
-  //             '<button class="btn btn-primary" data-time="' + workHours[i] + '"><i class="fa fa-save"></i></button>' +
-  //             '</div>' +
-  //             '</div>';
-  //         blocks += hourBlock;
-  //         console.log(hourBlock);
-
-  //     }
-  // }
-  //      $("#timeBlocks").prepend(blocks);
+//  End of document.ready 
 });
